4014d1836301f2ffcda2fee4831a67356904ea16
Ben Burdette <bburdette@gmail.com>
2019-10-15 13:15:25 -0600

    left and right mouse buttons, remove dead code

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,6 @@ extern crate touchpage;
 
 use failure::err_msg;
 use failure::Error as FError;
-use std::fs::File;
-use std::io::Read;
-use std::io::Write;
-use std::path::Path;
 use touchpage::control_nexus::{ControlNexus, ControlUpdateProcessor};
 use touchpage::control_updates as cu;
 use touchpage::controls::Orientation::{Horizontal, Vertical};
@@ -18,16 +14,6 @@ use touchpage::websocketserver;
 use inputbot::{MouseButton, MouseCursor};
 
 fn main() {
-  // when developing the elm code, I like to load the page from a file.  It requires
-  // restarting the server when the file changes.  At least you don't have to recompile
-  // the server though.
-  /*  let mbhtml = match load_string("index.html") {
-        Ok(html) => Some(html),
-        _ => None,
-      };
-  */
-
-  // html == None means use the precompiled elm/html in string_defaults.rs
   let mbhtml = None;
 
   let rootv: Result<String, FError> = build_gui()
@@ -42,9 +28,6 @@ fn main() {
       ERRORUI.to_string()
     }
   };
-
-  // see the json created above, if you want.
-  // write_string(guijson.as_str(), "json.txt");
 
   // the 'ControlUpdateProcessor' does something when an update message comes in.
   let cup = MouseUpdate { last_loc: None };
@@ -66,16 +49,15 @@ pub struct MouseUpdate {
 
 impl ControlUpdateProcessor for MouseUpdate {
   fn on_update_received(&mut self, update: &cu::UpdateMsg, cn: &mut ControlNexus) -> () {
-    let mousemult = 1000.0;
+    let mousemult = 1200.0;
     // println!("control update: {:?}", update);
     match update {
       cu::UpdateMsg::XY {
-        control_id,
+        control_id: _,
         state,
         location,
-        label,
+        label: _ ,
       } => {
-        println!("xy: {:?}", update);
         match location {
           Some((x, y)) => match self.last_loc {
             Some((lx, ly)) => {
@@ -88,7 +70,7 @@ impl ControlUpdateProcessor for MouseUpdate {
               self.last_loc = Some((*x, *y));
             }
             None => {
-              self.last_loc = Some((*x,*y));
+              self.last_loc = Some((*x, *y));
             }
           },
           None => (),
@@ -97,16 +79,30 @@ impl ControlUpdateProcessor for MouseUpdate {
           Some(cu::PressState::Unpressed) => self.last_loc = None,
           _ => (),
         };
-        println!("lastloc: {:?}", self.last_loc);
       }
-      cu::UpdateMsg::Button { control_id, .. } => {
+      cu::UpdateMsg::Button {
+        control_id, state, ..
+      } => {
+        let pr = match state {
+          Some(cu::PressState::Pressed) => true,
+          Some(cu::PressState::Unpressed) => false,
+          _ => false,
+        };
         cn.get_name(control_id).map(|name| {
           if name == "b0" {
             // left mouse.
-            println!("left mouse: {:?}", update);
+            if pr {
+              MouseButton::LeftButton.press()
+            } else {
+              MouseButton::LeftButton.release()
+            };
           } else if name == "b1" {
             // right mouse.
-            println!("right mouse: {:?}", update);
+            if pr {
+              MouseButton::RightButton.press()
+            } else {
+              MouseButton::RightButton.release()
+            };
           };
         });
         ()
@@ -143,17 +139,4 @@ const ERRORUI: &'static str = r##"
     }
 }"##;
 
-pub fn load_string(file_name: &str) -> Result<String, FError> {
-  let path = &Path::new(&file_name);
-  let mut inf = File::open(path)?;
-  let mut result = String::new();
-  inf.read_to_string(&mut result)?;
-  Ok(result)
-}
 
-pub fn write_string(text: &str, file_name: &str) -> Result<(), FError> {
-  let path = &Path::new(&file_name);
-  let mut inf = File::create(path)?;
-  inf.write(text.as_bytes())?;
-  Ok(())
-}
